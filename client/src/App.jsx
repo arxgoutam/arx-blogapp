c88cc40ca780c9938c246084e4b1aa54e9f7a5d9
arxgoutam <goutampaik42@gmail.com>
2024-10-24 17:45:39 +0600

    client pages routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,63 @@
 import { useState } from 'react'
 
+import {createBrowserRouter, RouterProvider, Outlet} from "react-router-dom";
+
 import './App.css'
 import Home from './pages/Home'
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
+import Single from './pages/Single';
+import Write from './pages/Write';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import ScrollToTop from './components/ScrollToTop';
+
+const Layout = ()=>{
+  return(
+    <>
+    <ScrollToTop/>
+    <Navigation/>
+    <Outlet/>
+    <Footer/>
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children:[
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/post/:id",
+        element: <Single/>
+      },
+      {
+        path: "/write",
+        element: <Write/>
+      }
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register/>
+  },
+  {
+    path: "/login",
+    element: <Login/>
+  }
+]);
+
 
 function App() {
 
   return (
     <>
-      <Home/>
+      <RouterProvider router={router} />
     </>
   )
 }
---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,12 @@
+
+import "./loader.scss"
+
+const Loader = ()=>{
+    return(
+        <div className="loader-con">
+            <div className="loader"></div>
+        </div>
+    )
+} 
+
+export default Loader;
