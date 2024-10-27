3334de8cdfd1fff86d213a491e1971f8d7ffd8c9
arxgoutam <goutampaik42@gmail.com>
2024-10-27 17:25:10 +0600

    register

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+const cookieparser = require('cookie-parser');
 
 const app = express();
 const userRouter = require('./router/userRouter');
 const authRouter = require('./router/authRouter');
 
-app.use(express.json())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
+app.use(express.json());
 
 app.use('/api/users', userRouter);
 app.use('/api/auth', authRouter);
