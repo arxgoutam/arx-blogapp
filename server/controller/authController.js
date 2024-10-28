fd9ef7bcc4dbc39ae1f5836edd64e2ce9f4a2731
arxgoutam <goutampaik42@gmail.com>
2024-10-28 11:07:08 +0600

    login controller - Generate JWT token

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,5 +1,5 @@
 
-
+const jwt = require('jsonwebtoken');
 const pool = require('../database');
 
 const authController = {
@@ -9,7 +9,7 @@ const authController = {
             const getQuery = "SELECT * FROM users WHERE username = ? OR email= ?";
             const [result] = await pool.query(getQuery, [req.body.username, req.body.email]);
             if(result.length){
-                return req.status(409).json("User already exist")
+                return res.status(409).json("User already exist")
             }
             // insert user 
             const insertQuery = "INSERT INTO users(`full_name`, `username`, `email`, `password`) VALUES (?, ?, ?, ?)";
@@ -18,6 +18,26 @@ const authController = {
             return res.status(200).json("User Created Successfully. ")
         } catch (error) {
             console.log(error);
+            return res.status(500).json("Server Side Problem")
+        }
+    },
+    login: async (req, res) => {
+        try {
+            // Check User 
+            const query = "SELECT * FROM users WHERE email = ?";
+            const [data] = await pool.query(query, [req.body.email]);
+
+            if(data.length === 0) return res.status(404).json("user not found");
+
+            // Check Psaaword
+            const isPasswordCorrect = req.body.password === data[0].password;
+            if(!isPasswordCorrect) return res.status(400).json("Wrong email or password");
+
+            // Generate JWT token
+            const token = jwt.sign({id: data[0].id}, "jwtkey");
+            const { password, ...other} = data[0]
+
+        } catch (error) {
             return res.status(500).json("Server Side Problem")
         }
     }
