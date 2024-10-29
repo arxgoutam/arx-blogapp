c750d0608cf78629fe0c517486f71d037a17ad0e
arxgoutam <goutampaik42@gmail.com>
2024-10-29 17:16:03 +0600

    postPost controller and write page post system

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,16 +1,74 @@
-
+import axios from "axios";
+import moment from "moment";
+import React, { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 const Write = () =>{
+    const state = useLocation.state
+    const navigate = useNavigate();
+    const [title, setTitle] = useState(state ? state.title : "");
+    const [desc, setDesc] = useState(state ? state.post_desc : "");
+    const [file, setFile] = useState(null);
+    const [cat, setCat] = useState(state ? state.cat : "");
+
+    const upload = async () => {
+        const formData = new FormData(); // Corrected the variable name here
+        formData.append("file", file); // Ensure `file` is correctly set from your input
+        try {
+          const res = await axios.post("http://localhost:8000/api/upload/posts", formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data', // Important to set correct headers
+            },
+          });
+          console.log(res.data)
+          return res.data
+        } catch (error) {
+          console.log(error);
+        }
+     
+      };
+
+      const handleClick = async (e) => {
+        e.preventDefault();
+        let imgUrl = "";
+        if(file) {
+            try {
+                imgUrl = await upload();
+            } catch (uploadError) {
+                console.log("error uploading image", uploadError);
+                return;
+            }
+        }
+        try {
+            const payload = {
+                title,
+                desc,
+                cat,
+                img: file ? imgUrl : state.img,
+                date: moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+            };
+            if(state){
+                // Update Post
+            } else {
+                // Create new post 
+                const response = await axios.post(`http://localhost:8000/api/posts`, payload, { withCredentials: true });
+                console.log("post created:", response.data);
+            }
+        } catch (error) {
+            console.error("Error during post operation arx:", error);
+        }
+      };
+
     return(
         <div className="write-page">
         <div className="write-container">
             <div className="write-left">
                 <div className="title">
                     <p>Title</p>
-                    <input type="text" placeholder="Title Here"/>
+                    <input type="text" placeholder="Title Here" value={title} onChange={e=>setTitle(e.target.value)}/>
                 </div>
                 <div className="description">
                     <p>Description</p>
-                    <textarea name="" id="" rows={10}></textarea>
+                    <textarea name="" id="" rows={20} value={desc} onChange={e=>setDesc(e.target.value)}></textarea>
                 </div>
             </div>
             <div className="write-right">
@@ -23,34 +81,34 @@ const Write = () =>{
                         <b>Visibility: </b> Public
                     </span><br />
                     <div className="upload-img">
-                        <input type="file" id="file"/>
+                        <input type="file" id="file" onChange={e=>setFile(e.target.files[0])}/>
                         <label htmlFor="file">Upload Image</label>
                     </div>
                     <div className="btn-group">
                         <button className="draft-btn">Save As Draft</button>
-                        <button className="publish-btn">Publish</button>
+                        <button className="publish-btn" onClick={handleClick}>Publish</button>
                     </div>
                 </div>
                 <div className="box">
                     <h1>Category</h1>
                     <div className="cat">
-                        <input type="radio" id="blockchain" name="cat" value="blockchain"/>
+                        <input checked={cat === "blockchain"} type="radio" id="blockchain" name="cat" value="blockchain" onChange={e=>setCat(e.target.value)}/>
                         <label htmlFor="blockchain">Blockchain</label>
                     </div>
                     <div className="cat">
-                        <input type="radio" id="trading" name="cat" value="trading"/>
+                        <input checked={cat === "trading"} type="radio" id="trading" name="cat" value="trading" onChange={e=>setCat(e.target.value)}/>
                         <label htmlFor="trading">Trading</label>
                     </div>
                     <div className="cat">
-                        <input type="radio" id="crypto" name="cat" value="crypto"/>
+                        <input checked={cat === "crypto"} type="radio" id="crypto" name="cat" value="crypto" onChange={e=>setCat(e.target.value)}/>
                         <label htmlFor="crypto">Crypto</label>
                     </div>
                     <div className="cat">
-                        <input type="radio" name="cat" id="nft" value="nft"/>
+                        <input checked={cat === "nft"} type="radio" name="cat" id="nft" value="nft" onChange={e=>setCat(e.target.value)}/>
                         <label htmlFor="nft">NFT</label>
                     </div>
                     <div className="cat">
-                        <input type="radio" name="cat" id="ai" value="ai"/>
+                        <input checked={cat === "ai"} type="radio" name="cat" id="ai" value="ai" onChange={e=>setCat(e.target.value)}/>
                         <label htmlFor="ai">AI</label>
                     </div>
                 </div>
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const path = require('path');
 
 const app = express();
 require('dotenv').config();
@@ -19,6 +21,43 @@ app.use(cookieParser());
 app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
 app.use('/api/posts', postRouter);
+
+
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads/posts')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + " " + file.originalname)
+    }
+  })
+  
+  // Set up multer to store uploaded files in the "uploads" directory
+  const upload = multer({ storage})
+
+// Serve static files from the "uploads" folder
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+app.post('/api/upload/posts', upload.single('file'), (req, res) => {
+  try {
+    const file = req.file;
+    if (!file) {
+      return res.status(400).json("No file uploaded.");
+    }
+     // Construct the file URL (to be accessible on the client side)
+     const fileUrl = `http://localhost:8000/uploads/posts/${file.filename}`;
+
+    // res.status(200).json(file.filename); // Return the uploaded file's name
+    res.status(200).json(fileUrl);
+  } catch (error) {
+    res.status(500).json("Server error during upload.");
+  }
+});
+
+
+
 
 app.get('/', (req, res)=>{
     res.send("Server Running")
